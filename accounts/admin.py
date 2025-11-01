141fd5e4859676b2c11750e9437731ddee371dd1
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-01 13:29:19 +0545

    read only fields date joined

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,6 +5,8 @@ from .models import Account
 # Register your models here.
 class AccountAdmin(UserAdmin):
     list_display=('email','first_name','last_name','date_joined','last_login','is_active',)
+    list_display_links=('email','first_name')
+    readonly_fields = ('date_joined','last_login')
     filter_horizontal=()
     list_filter = ()
     fieldsets = ()

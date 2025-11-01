8ff64c18c7534fdf24e90ecb2df99d7678dd35b2
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-01 13:35:58 +0545

    read only fields date joined

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,6 +7,7 @@ class AccountAdmin(UserAdmin):
     list_display=('email','first_name','last_name','date_joined','last_login','is_active',)
     list_display_links=('email','first_name')
     readonly_fields = ('date_joined','last_login')
+    ordering = ('-date_joined',)
     filter_horizontal=()
     list_filter = ()
     fieldsets = ()

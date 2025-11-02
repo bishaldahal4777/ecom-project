b48de95a42a58a5d974bb20d0ea74d9aefbe5edd
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-02 07:19:25 +0545

    fixing admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,15 +3,4 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
 # Register your models here.
-class AccountAdmin(UserAdmin):
-    list_display=('email','first_name','last_name','date_joined','last_login','is_active',)
-    list_display_links=('email','first_name')
-    readonly_fields = ('date_joined','last_login')
-    ordering = ('-date_joined',)
-    filter_horizontal=()
-    list_filter = ()
-    fieldsets = ()
 
-
-
-admin.site.register(Account,AccountAdmin)

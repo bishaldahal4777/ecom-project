58b096c4dc30aab7becc5048d0363e0c951196bb
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-02 20:52:14 +0545

    new commit

--- SajiloKart/urls.py
+++ SajiloKart/urls.py
@@ -22,5 +22,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('',views.home, name='home'),
+    path('',views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

4444df1ca251ff16748c299279f183d93c8d27a5
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-02 18:09:26 +0545

    media fix

--- SajiloKart/settings.py
+++ SajiloKart/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # media files configuration
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

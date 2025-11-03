d7a70a7187b0e239e70008cc52fe50ca0d90fb23
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-03 11:52:18 +0545

    store models

--- store/models.py
+++ store/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    product_name = models.CharField(max_length=200, unique=True)
+    slug=models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+    
     

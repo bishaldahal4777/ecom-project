97bf7b8520115b3d929129f0f09691c8e9e26d9e
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-04 21:15:44 +0545

    admin site registerd

--- store/admin.py
+++ store/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Product
 # Register your models here.
 
+class ProductAdmin(models.ModelAdmin):
+    list_display =('product_name','price','stock','category','modified_date')
+
 admin.site.register(Product)

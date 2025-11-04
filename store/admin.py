255d3d2b8485d8b5058e0c2d6d6876924f4fd1e4
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-04 21:18:31 +0545

    fix admin

--- store/admin.py
+++ store/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Product
 # Register your models here.
 
-class ProductAdmin(models.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):
     list_display =('product_name','price','stock','category','modified_date','is_available')
     prepopulated_fields = {'slug': ('product_name',)}
 

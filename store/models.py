8ce449d6f2ae5ca033156a30ff9de8162b663815
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-04 21:10:33 +0545

    created date added in models.py

--- store/models.py
+++ store/models.py
@@ -11,3 +11,4 @@ class Product(models.Model):
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)

c3451e1eb84aaabc95f9112a9669f4c0f626d96d
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-27 19:23:26 +0545

    new commit

--- category/admin.py
+++ category/admin.py
@@ -6,5 +6,3 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields={'slug':('category_name',)}
     list_display = ('category_name', 'slug')
 
-
-admin.site.register(Category, CategoryAdmin)
